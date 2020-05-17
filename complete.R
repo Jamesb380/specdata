835fc0aa51ac1e64bc37afa1fbb082775035ac96
jamesb380 <jamesbnbennett@gmail.com>
2020-05-17 15:58:09 -0400

    returns table of names and number of obs per file

---
+++ complete.R
@@ -0,0 +1,20 @@
+
+
+
+
+complete <- function(directory, id = 1:332){
+        
+        df_stat <- file.input2(directory, id)
+}
+
+
+
+setwd("/Users/james.bennett/Desktop/specdata")
+
+####Load Input Handler
+source("./Input_handler.R")
+
+all.files <- 1:332
+mi.dir <- "/Users/james.bennett/Desktop/specdata/data/specdata"
+
+complete.stats <- complete(directory = mi.dir, id = all.files)
