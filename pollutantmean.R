4062a329ebe3a31274eec6206fc0f951793ca76e
jamesb380 <jamesbnbennett@gmail.com>
2020-06-19 10:08:51 -0400

    finalized function

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,27 +2,15 @@ library(plyr)
 library(readr)
 
 
-
-
-pollutantmean <- function(directory, pollutant, id = 1:332){ 
+pollutantmean <- function(directory, pollutant, id = 1:332){
+       
+         setwd("/Users/james.bennett/Desktop/specdata")
+               
+        source("./Input_handler.R")
         df_extract <- file.inputl(directory, id)
-        
         pollutant.mean <- mean(df_extract[[pollutant]],na.rm = T) 
-        
+        pollutant.mean
 }
 
-setwd("/Users/james.bennett/Desktop/specdata")
-
-####Load Input Handler
-source("./Input_handler.R")
-
-### Define function arguments ###
-
-mi.dir <- "/Users/james.bennett/Desktop/specdata/data/specdata"
-mi.id <- 15:40
-el.pollutant <- "sulfate"
 
 
-####################################
-
-mean.pollutant <- pollutantmean(directory = mi.dir,pollutant = el.pollutant, id = mi.id)
