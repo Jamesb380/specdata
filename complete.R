7bfb60467a24be3a9d4a107ea5676c19cc980dd1
jamesb380 <jamesbnbennett@gmail.com>
2020-06-19 10:09:53 -0400

    finalized function

--- complete.R
+++ complete.R
@@ -5,18 +5,15 @@ library(readr)
 
 
 complete <- function(directory, id = 1:332){
-        
-        df_stat <- file.input2(directory, id)
+     
+     df_extract <- file.inputl(directory, id)
+     blanks <- which(is.na(df_extract$nitrate))
+     df_extract <- df_extract[-blanks,]
+     sags <- which(is.na(df_extract$sulfate))
+     df_extract <- df_extract[-sags,]
+     count(df_extract$ID)
+     
 }
 
 
 
-setwd("/Users/james.bennett/Desktop/specdata")
-
-####Load Input Handler
-source("./Input_handler.R")
-
-all.files <- 1:332
-mi.dir <- "/Users/james.bennett/Desktop/specdata/data/specdata"
-
-complete.stats <- complete(directory = mi.dir, id = all.files)
